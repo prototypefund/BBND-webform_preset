deb76107c2ed7d4115213914ae18bfdf12e2660e
Merlin <merlin@geeks4change.net>
2023-02-04 16:17:11 +0100

    Implement element access

--- src/Plugin/WebformHandler/WebformPresetWebformHandler.php
+++ src/Plugin/WebformHandler/WebformPresetWebformHandler.php
@@ -4,10 +4,14 @@ declare(strict_types=1);
 
 namespace Drupal\webform_preset\Plugin\WebformHandler;
 
+use Drupal\Core\Access\AccessResult;
 use Drupal\Core\Cache\CacheableMetadata;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Http\Exception\CacheableAccessDeniedHttpException;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\webform\Entity\Webform;
 use Drupal\webform\Plugin\WebformHandlerBase;
+use Drupal\webform\WebformInterface;
 use Drupal\webform\WebformSubmissionInterface;
 use Drupal\webform_preset\Entity\WebformPreset;
 
@@ -23,11 +27,12 @@ use Drupal\webform_preset\Entity\WebformPreset;
  */
 final class WebformPresetWebformHandler extends WebformHandlerBase {
 
+  protected $presets = [];
+
   public function prepareForm(WebformSubmissionInterface $webform_submission, $operation, FormStateInterface $form_state) {
     if ($operation === 'add') {
-      $webformPreset = WebformPreset::loadByRequestQuery($webform_submission->getWebform());
-      $data = $webformPreset ? $webformPreset->getData() : NULL;
-      if ($data) {
+      $data = $this->getWebformPresetData($webform_submission->getWebform());
+      if (isset($data)) {
         $webform_submission->setData($data);
       }
       else {
@@ -39,6 +44,28 @@ final class WebformPresetWebformHandler extends WebformHandlerBase {
         throw new CacheableAccessDeniedHttpException($cacheability);
       }
     }
+  }
+
+  public function accessElement(array &$element, $operation, AccountInterface $account = NULL) {
+    $accessResult = AccessResult::neutral();
+    if ($operation === 'create') {
+      $data = $this->getWebformPresetData($this->webform);
+      if (isset($data)) {
+        $key = $element['#webform_key'];
+        // Neutral means allowed, so can not use ::allowedIf(). Sigh.
+        $accessResult = AccessResult::forbiddenIf(isset($data[$key]));
+      }
+    }
+    return $accessResult;
+  }
+
+  public function getWebformPresetData(WebformInterface $webform): ?array {
+    $cacheId = $webform->id();
+    if (!array_key_exists($cacheId, $this->presets)) {
+      $webformPreset = WebformPreset::loadByRequestQuery($webform);
+      $this->presets[$cacheId] = $webformPreset ? $webformPreset->getData() : NULL;
+    }
+    return $this->presets[$cacheId];
   }
 
 }
